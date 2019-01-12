4cc49e1fcc342a676f919dafd1b99e2c6092f700
Grace0 <gracekwak25@gmail.com>
2019-01-12 14:19:24 -0800

    Takes center point depth

---
+++ stream.py
@@ -0,0 +1,27 @@
+import pyrealsense2 as rs
+from networktables import NetworkTables
+
+try:
+    # Create a context object. This object owns the handles to all connected realsense devices
+    pipeline = rs.pipeline()
+    pipeline.start()
+
+    # This call waits until a new coherent set of frames is available on a device
+    # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+    while True:
+        frames = pipeline.wait_for_frames()
+        depth = frames.get_depth_frame()
+        if not depth:
+            continue
+        break
+    dist = depth.get_distance(640, 360)
+    print(dist)
+    exit(0)
+#except rs.error as e:
+#    # Method calls agaisnt librealsense objects may throw exceptions of type pylibrs.error
+#    print("pylibrs.error was thrown when calling %s(%s):\n", % (e.get_failed_function(), e.get_failed_args()))
+#    print("    %s\n", e.what())
+#    exit(1)
+except Exception as e:
+    print(e)
+    pass
