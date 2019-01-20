60c9d50dfd88bb9fda0ce3a53bfe2e780c6575a1
Grace0 <gracekwak25@gmail.com>
2019-01-20 12:08:59 -0800

    Showing blobs

--- stream.py
+++ stream.py
@@ -3,6 +3,9 @@ from networktables import NetworkTables
 import threading
 import numpy as np
 import cv2
+import grip
+
+grip_pipeline = grip.HatchPipeline()
 
 #will not need to pass anything but a bool to network Tables
 
@@ -46,11 +49,12 @@ try:
             continue
         depth_image = np.asanyarray(depth.get_data())
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        grip_pipeline.process(depth_colormap)
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', depth_colormap)
+        #show_img = np.asanyarray(grip_pipeline.find_blobs_output)
+        cv2.imshow('RealSense', grip_pipeline.hsl_threshold_output)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite('test.png',depth_colormap)
             break
 
 finally:
