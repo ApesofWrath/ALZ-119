a096074a8cff7f060e41060d6a9d999e3081ed94
Brian Hardell <bharde@me.com>
2019-02-03 16:31:09 -0800

    proof of concept, works; need to tune for different camera and adjust values when implement

--- hatchdetection.py
+++ hatchdetection.py
@@ -2,20 +2,31 @@ import cv2, numpy, math
 
 
 class HatchDetection:
-	def __init__(self, pipe, width, height):
+	def __init__(self, pipe):
 		self.pipe = pipe  # the object from the grip file
 		self.img = None
 		self.is_hatch = False
 
+	def generate_num_contours(self, contour_data):
+		counter = 0
+		for contour in contour_data:
+			if cv2.contourArea(contour) < 200: # <--play with this value to change the tollerance
+				counter += 1
+		return counter
+
 	def update(self, im):
 		self.pipe.process(im)
-		self.img = im
+		self.img = self.pipe.hsv_threshold_output
+
 		contour_data = self.pipe.find_contours_output
+		for contour in contour_data:
+			print(cv2.contourArea(contour))
+		print()
 
-		if len(contour_data) >= 1:
+		if self.generate_num_contours(contour_data) >= 3: # <--play with this value to change the tollerance
 			self.is_hatch = True
+		else:
+			self.is_hatch = False
 
+		# print(self.is_hatch)
 		cv2.imshow("CONTOUR", self.img)
-
-
-
---
+++ main.py
@@ -0,0 +1,22 @@
+import grip, cv2, numpy, hatchdetection
+import math
+import sys
+
+cap = cv2.VideoCapture(1)
+pipe = grip.GripPipeline()
+detection = hatchdetection.HatchDetection(pipe)
+
+if not cap.isOpened():
+    cap.open()
+
+def main():
+    while True:
+        ret, im = cap.read()
+        detection.update(im)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
